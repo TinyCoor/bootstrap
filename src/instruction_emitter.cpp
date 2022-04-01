475212eea0997f60310fada2e265a01dd9e144d6
TinyEnd <rowbert121@outlook.com>
2022-04-01 22:07:40 +0800

    fix add instruction encode

--- src/instruction_emitter.cpp
+++ src/instruction_emitter.cpp
@@ -5,7 +5,8 @@
 #include "instruction_emitter.h"
 namespace gfx {
 
-size_t instruction_emitter::size() const {
+size_t instruction_emitter::size() const
+{
 	size_t size = 0;
 	for (const auto& inst : inst_) {
 		size += inst.encoding_size();
@@ -13,33 +14,53 @@ size_t instruction_emitter::size() const {
 	return size;
 }
 
-void instruction_emitter::nop() {
+void instruction_emitter::nop()
+{
 	instruction_t no_op;
 	no_op.op = op_codes::nop;
 	inst_.emplace_back(no_op);
 }
 
-void instruction_emitter::rts() {
+void instruction_emitter::dup()
+{
+	instruction_t dup_op;
+	dup_op.op = op_codes::dup;
+	inst_.emplace_back(dup_op);
+}
+
+void instruction_emitter::rts()
+{
 	instruction_t rts_op;
 	rts_op.op = op_codes::rts;
 	inst_.emplace_back(rts_op);
 }
 
-void instruction_emitter::exit() {
+void instruction_emitter::trap(uint8_t index)
+{
+	instruction_t trap_op;
+	trap_op.op = op_codes::trap;
+	trap_op.operands_count = 1;
+	trap_op.operands[0].type = operand_types::constant_integer;
+	trap_op.operands[0].value.u64 = index;
+	inst_.push_back(trap_op);
+}
+
+void instruction_emitter::exit()
+{
 	instruction_t exit_op;
 	exit_op.op = op_codes::exit;
 	inst_.emplace_back(exit_op);
 }
 
 void instruction_emitter::add_int_register_to_register(op_sizes size, uint8_t target_index, uint8_t lhs_index,
-													   uint8_t rhs_index)
+	uint8_t rhs_index)
 {
 	instruction_t add_op;
 	add_op.op = op_codes::add;
 	add_op.size= size;
 	add_op.operands_count = 3;
-	add_op.operands[1].index = target_index;
-	add_op.operands[1].type = operand_types::register_integer;
+	add_op.operands[0].index = target_index;
+	add_op.operands[0].type = operand_types::register_integer;
 	add_op.operands[1].index = lhs_index;
 	add_op.operands[1].type = operand_types::register_integer;
 	add_op.operands[2].index = rhs_index;
@@ -47,11 +68,11 @@ void instruction_emitter::add_int_register_to_register(op_sizes size, uint8_t ta
 	inst_.emplace_back(add_op);
 }
 
-void instruction_emitter::load_stack_offset_to_register(uint8_t target_index, uint64_t offset)
+void instruction_emitter::load_stack_offset_to_register(op_sizes size ,uint8_t target_index, uint64_t offset)
 {
 	instruction_t load_op;
 	load_op.op = op_codes::load;
-	load_op.size = op_sizes::qword;
+	load_op.size = size;
 	load_op.operands_count = 3;
 	load_op.operands[0].type = operand_types::register_integer;
 	load_op.operands[0].index = target_index;
@@ -62,10 +83,11 @@ void instruction_emitter::load_stack_offset_to_register(uint8_t target_index, ui
 	inst_.emplace_back(load_op);
 }
 
-void instruction_emitter::store_register_to_stack_offset(uint8_t source_index, uint64_t offset) {
+void instruction_emitter::store_register_to_stack_offset(op_sizes size ,uint8_t source_index, uint64_t offset)
+{
 	instruction_t store_op;
 	store_op.op = op_codes::store;
-	store_op.size = op_sizes::qword;
+	store_op.size = size;
 	store_op.operands_count = 3;
 	store_op.operands[0].type = operand_types::register_integer;
 	store_op.operands[0].index = source_index;
@@ -91,7 +113,8 @@ void instruction_emitter::multiply_int_register_to_register(op_sizes size, uint8
 	inst_.emplace_back(mul_op);
 }
 
-void instruction_emitter::move_int_constant_to_register(op_sizes size, uint64_t value, uint8_t index) {
+void instruction_emitter::move_int_constant_to_register(op_sizes size, uint64_t value, uint8_t index)
+{
 	instruction_t move_op;
 	move_op.op = op_codes::move;
 	move_op.size = size;
@@ -103,7 +126,8 @@ void instruction_emitter::move_int_constant_to_register(op_sizes size, uint64_t 
 	inst_.emplace_back(move_op);
 }
 
-void instruction_emitter::pop_float_register(uint8_t index) {
+void instruction_emitter::pop_float_register(uint8_t index)
+{
 	instruction_t pop_op;
 	pop_op.op = op_codes::pop;
 	pop_op.operands_count = 1;
@@ -112,7 +136,8 @@ void instruction_emitter::pop_float_register(uint8_t index) {
 	inst_.emplace_back(pop_op);
 }
 
-void instruction_emitter::push_float_constant(double value) {
+void instruction_emitter::push_float_constant(double value)
+{
 	instruction_t push_op;
 	push_op.op = op_codes::push;
 	push_op.operands_count = 1;
@@ -121,10 +146,10 @@ void instruction_emitter::push_float_constant(double value) {
 	inst_.emplace_back(push_op);
 }
 
-void instruction_emitter::jump_direct(uint64_t address) {
+void instruction_emitter::jump_direct(uint64_t address)
+{
 	instruction_t jmp_op;
 	jmp_op.op = op_codes::jmp;
-	jmp_op.size = op_sizes::qword;
 	jmp_op.operands_count = 1;
 	jmp_op.operands[0].type = operand_types::constant_integer;
 	jmp_op.operands[0].value.u64 = address;
@@ -142,17 +167,18 @@ void instruction_emitter::jump_subroutine_indirect(uint8_t index)
 	inst_.emplace_back(jsr_op);
 }
 
-void instruction_emitter::jump_subroutine_direct(uint64_t address) {
+void instruction_emitter::jump_subroutine_direct(uint64_t address)
+{
 	instruction_t jsr_op;
 	jsr_op.op = op_codes::jsr;
-	jsr_op.size = op_sizes::qword;
 	jsr_op.operands_count = 1;
 	jsr_op.operands[0].type = operand_types::constant_integer;
 	jsr_op.operands[0].value.u64 = address;
 	inst_.emplace_back(jsr_op);
 }
 
-void instruction_emitter::pop_int_register(op_sizes size, uint8_t index) {
+void instruction_emitter::pop_int_register(op_sizes size, uint8_t index)
+{
 	instruction_t pop_op;
 	pop_op.op = op_codes::pop;
 	pop_op.size = size;
@@ -162,7 +188,8 @@ void instruction_emitter::pop_int_register(op_sizes size, uint8_t index) {
 	inst_.emplace_back(pop_op);
 }
 
-void instruction_emitter::push_int_register(op_sizes size, uint8_t index) {
+void instruction_emitter::push_int_register(op_sizes size, uint8_t index)
+{
 	instruction_t push_op;
 	push_op.op = op_codes::push;
 	push_op.size = size;
@@ -172,7 +199,8 @@ void instruction_emitter::push_int_register(op_sizes size, uint8_t index) {
 	inst_.emplace_back(push_op);
 }
 
-void instruction_emitter::push_int_constant(op_sizes size, uint64_t value) {
+void instruction_emitter::push_int_constant(op_sizes size, uint64_t value)
+{
 	instruction_t push;
 	push.op = op_codes::push;
 	push.size = size;
@@ -182,11 +210,12 @@ void instruction_emitter::push_int_constant(op_sizes size, uint64_t value) {
 	inst_.emplace_back(push);
 }
 
-void instruction_emitter::load_with_offset_to_register(uint8_t source_index, uint8_t target_index, uint64_t offset)
+void instruction_emitter::load_with_offset_to_register(op_sizes size ,uint8_t source_index,
+	uint8_t target_index,uint64_t offset)
 {
 	instruction_t load_op;
 	load_op.op = op_codes::load;
-	load_op.size = op_sizes::qword;
+	load_op.size = size;
 	load_op.operands_count = 3;
 	load_op.operands[0].type = operand_types::register_integer;
 	load_op.operands[0].index = target_index;
@@ -197,7 +226,8 @@ void instruction_emitter::load_with_offset_to_register(uint8_t source_index, uin
 	inst_.push_back(load_op);
 }
 
-bool instruction_emitter::encode(result &r, terp &terp) {
+bool instruction_emitter::encode(result &r, terp &terp)
+{
 	size_t offset = 0;
 	for (auto& inst : inst_ ) {
 		auto inst_size = inst.encode(r, terp.heap(), start_address_ + offset);
@@ -209,7 +239,8 @@ bool instruction_emitter::encode(result &r, terp &terp) {
 	return true;
 }
 
-void instruction_emitter::dec(op_sizes size, uint8_t index) {
+void instruction_emitter::dec(op_sizes size, uint8_t index)
+{
 	instruction_t dec_op;
 	dec_op.op = op_codes::dec;
 	dec_op.operands_count = 1;
@@ -219,7 +250,8 @@ void instruction_emitter::dec(op_sizes size, uint8_t index) {
 	inst_.emplace_back(dec_op);
 }
 
-void instruction_emitter::inc(op_sizes size, uint8_t index) {
+void instruction_emitter::inc(op_sizes size, uint8_t index)
+{
 	instruction_t inc_op;
 	inc_op.op = op_codes::dec;
 	inc_op.operands_count = 1;
@@ -229,11 +261,12 @@ void instruction_emitter::inc(op_sizes size, uint8_t index) {
 	inst_.emplace_back(inc_op);
 }
 
-void instruction_emitter::store_with_offset_from_register(uint8_t source_index, uint8_t target_index, uint64_t offset)
+void instruction_emitter::store_with_offset_from_register(op_sizes size ,uint8_t source_index,
+	uint8_t target_index, uint64_t offset)
 {
 	instruction_t store_op;
 	store_op.op = op_codes::store;
-	store_op.size = op_sizes::qword;
+	store_op.size = size;
 	store_op.operands_count = 3;
 	store_op.operands[0].type = operand_types::register_integer;
 	store_op.operands[0].index = source_index;
@@ -305,7 +338,8 @@ void instruction_emitter::compare_int_register_to_constant(op_sizes sizes, uint8
 	inst_.push_back(compare_op);
 }
 
-void instruction_emitter::compare_int_register_to_register(op_sizes size, uint8_t lhs_index, uint8_t rhs_index) {
+void instruction_emitter::compare_int_register_to_register(op_sizes size, uint8_t lhs_index, uint8_t rhs_index)
+{
 	instruction_t compare_op;
 	compare_op.op = op_codes::cmp;
 	compare_op.size = size;
@@ -317,25 +351,24 @@ void instruction_emitter::compare_int_register_to_register(op_sizes size, uint8_
 	inst_.push_back(compare_op);
 }
 
-void instruction_emitter::branch_if_equal(uint64_t address) {
+void instruction_emitter::branch_if_equal(uint64_t address)
+{
 	instruction_t branch_op;
 	branch_op.op = op_codes::beq;
-	branch_op.size = op_sizes::qword;
 	branch_op.operands_count = 1;
 	branch_op.operands[0].type = operand_types::constant_integer;
 	branch_op.operands[0].value.u64 = address;
 	inst_.push_back(branch_op);
 }
 
-void instruction_emitter::branch_if_not_equal(uint64_t address) {
+void instruction_emitter::branch_if_not_equal(uint64_t address)
+{
 	instruction_t branch_op;
 	branch_op.op = op_codes::bne;
-	branch_op.size = op_sizes::qword;
 	branch_op.operands_count = 1;
 	branch_op.operands[0].type = operand_types::constant_integer;
 	branch_op.operands[0].value.u64 = address;
 	inst_.push_back(branch_op);
 }
-
 
 }

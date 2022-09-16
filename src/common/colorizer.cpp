04b0b17bde6ecc73e95e7fa98d938a771f4b40aa
TinyEnd <rowbert121@outlook.com>
2022-09-16 22:29:39 +0800

    linux colorize

--- src/common/colorizer.cpp
+++ src/common/colorizer.cpp
@@ -1,31 +1,50 @@
 //
 // Created by 12132 on 2022/9/11.
 //
-
 #include "colorizer.h"
 #include <fmt/format.h>
 #include <sstream>
 
 namespace gfx {
-std::string gfx::colorizer::colorize_range(const std::string &text,
-                                                     size_t begin,
-                                                     size_t end,
-                                                     gfx::term_colors_t color) noexcept
+std::string colorizer::colorize_range(const std::string &text, size_t begin, size_t end,
+     term_colors_t fg_color, term_colors_t bg_color) noexcept
 {
-    std::stringstream  color_stream;
+    std::stringstream color_stream;
     for (size_t i = 0; i < text.length() ; ++i) {
-        if (i == begin) {
-            color_stream << fmt::format("\033[1;{}m", (uint32_t)color);
-        } else if (i == end) {
-            color_stream << "\033[0m";
+        if (i == begin && begin == end) {
+            color_stream << fmt::format("{}{}", color_code(make_bg_color(bg_color)), color_code(fg_color));
+            color_stream << text[i];
+            color_stream << color_code_reset();
+        } else {
+            if (i == begin) {
+                color_stream << fmt::format("{}{}", color_code(make_bg_color(bg_color)), color_code(fg_color));
+            } else if (i == end){
+                color_stream << color_code_reset();
+            }
+            color_stream << text[i];
         }
-        color_stream << text[i];
     }
     return color_stream.str();
 }
 
-std::string colorizer::colorize(const std::string &text, term_colors_t color)
+std::string colorizer::colorize(const std::string &text, term_colors_t fg_color, term_colors_t bg_color)
 {
-    return fmt::format("\033[1;{}m{}\033[0m", (uint32_t)color, text);
+    return fmt::format("{}{}{}{}", color_code(make_bg_color(bg_color)), color_code(fg_color), text, color_code_reset());
 }
+
+std::string colorizer::color_code(term_colors_t color)
+{
+    return fmt::format("\033[1;{}m", (uint32_t)color);
+}
+
+constexpr const char *colorizer::color_code_reset()
+{
+    return "\033[0m";
+}
+
+constexpr term_colors_t colorizer::make_bg_color(term_colors_t color)
+{
+    return static_cast<term_colors_t>((uint32_t)color + 10);
+}
+
 }
--- src/common/colorizer.h
+++ src/common/colorizer.h
@@ -18,14 +18,18 @@ enum class term_colors_t : uint8_t {
     white,
 };
 
-
 class colorizer {
 public:
+    static  std::string color_code(term_colors_t color);
+
+    static constexpr const char *color_code_reset();
+
+    static constexpr term_colors_t make_bg_color(term_colors_t color);
 
     static std::string colorize_range(const std::string &text,
-        size_t begin, size_t end, term_colors_t color) noexcept;
+        size_t begin, size_t end, term_colors_t fg_color, term_colors_t bg_color =term_colors_t::black) noexcept;
 
-    static std::string colorize(const std::string &text, term_colors_t color);
+    static std::string colorize(const std::string &text, term_colors_t fg_color, term_colors_t bg_color =term_colors_t::black);
 
 private:
 
